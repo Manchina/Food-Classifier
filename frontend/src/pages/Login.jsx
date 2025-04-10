efd55176d37a967128c0ecacd1d2c0828bc6b965
MANCHINA PREM KUMAR <104674915+Manchina@users.noreply.github.com>
2025-04-10 18:45:52 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
       formData.append('username', username);
       formData.append('password', password);
 
-      const response = await axios.post('http://localhost:8000/login', formData, {
+      const response = await axios.post('https://food-classifier-ihbm.onrender.com/login', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
